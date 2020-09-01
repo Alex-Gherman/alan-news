587a88fdacf8ee72fec56f9c5636960c95f1e00c
Alex-Gherman <alexgherman68@gmail.com>
2020-09-01 02:13:40 +0200

    alan project is done just the Alan key u need to put

--- src/App.js
+++ src/App.js
@@ -1,14 +1,57 @@
-import React,{ useEffect } from 'react';
+import React,{  useEffect } from 'react';
 import alanBtn from "@alan-ai/alan-sdk-web";
+import NewsCards from './components/NewsCards/NewsCards'
+import useStyles from './style'
+import wordsToNumbers from 'words-to-numbers'
+require('dotenv').config()
 
 
 
 
+const key = ""
 const App = () =>{
-  useEffect( ()=> {},[] )
+  const [newsArticles,setNewsArticles] = React.useState([])
+  const [activArticle,setActivArticle] = React.useState(-1)
+  const classes = useStyles()
+
+  useEffect( ()=> {
+    alanBtn({
+
+      key:key,
+      onCommand: ({ command ,articles, number}) => {
+        if(command === "newHeadlines"){
+          setNewsArticles(articles);
+          setActivArticle(-1);
+        }
+        else if(command === "highlight"){
+          setActivArticle( prevAvtiveArticle => prevAvtiveArticle +1 )
+        }
+        else if(command === "open"){
+          const parseNumber = number.length > 2 ? wordsToNumbers(number, {fuzzy: true}) : number;
+          const article = articles[parseNumber -1]
+
+          if(parseNumber >20 ){
+            alanBtn().playText('Please try again')
+          }
+          else if (articles){
+            alanBtn().playText('Opening...')
+            window.open(article.url, '_blank');
+
+          }
+          
+        }
+        
+      }
+
+    })
+  },[] )
+  const alanLogoSrc = 'https://alan.app/voice/images/previews/preview.jpg';
   return(
     <div>
-      <h1>Alan Ai News Aplication</h1>
+      <div className={classes.logoContainer}>
+        <img src={alanLogoSrc} className={classes.alanLogo} alt="alan logo"/>
+      </div>
+      <NewsCards articles={newsArticles} activArticle={activArticle} />
     </div>
   );
 }
---
+++ src/components/NewsCard/NewsCard.js
@@ -0,0 +1,65 @@
+import React,{useEffect,createRef} from 'react';
+import { Card, CardActionArea, CardMedia, Typography, CardContent, CardActions, Button} from '@material-ui/core';
+import useStyles from './styles';
+import classNames from 'classnames';
+
+
+const Newscard = ({ 
+    articles: {
+    description,
+    publishedAt,
+    source,
+    title,
+    url,
+    urlToImage} , 
+    i,
+    activArticle
+}) => {
+
+    const classes = useStyles()
+
+    const [elRefs, setElRefs] = React.useState([]);
+    const scrollToRef = ref => window.scroll(0,ref.current.offsetTop - 50);
+    useEffect( ()=>{
+        setElRefs( refs => Array(20).fill().map( (_,j ) => refs[j] || createRef() ))
+    },[])
+
+    useEffect( ()=>{
+        if(i === activArticle && elRefs[activArticle]){
+            scrollToRef(elRefs[activArticle]);
+        }
+    },[i, activArticle, elRefs])
+
+    return (
+        <Card ref={elRefs[i]} className={classNames(classes.card, activArticle === i ? classes.activeCard:null)}>
+            <CardActionArea href={url} target="_blank">
+                <CardMedia className={classes.media} image={urlToImage || 'https://cdn.pixabay.com/photo/2015/02/15/09/33/news-636978_960_720.jpg'}/>
+                <div className={classes.details}>
+                    <Typography variant="body2" component="h2" color="textSecondary">
+                        {(new Date(publishedAt)).toDateString()}
+                    </Typography>
+                    <Typography variant="body2" component="h2" color="textSecondary">
+                        {source.name}
+                    </Typography>
+                </div>
+                    <Typography className={classes.title} gutterBottom variant="h5">
+                        {title}
+                    </Typography>
+                    <CardContent>
+                        <Typography variant="body2" color="textPrimary" component="p">
+                            {description}
+                        </Typography>
+                    </CardContent>
+            </CardActionArea>
+            <CardActions className={classes.cardActions}>
+                <Button size="small" color="primary">Learne More</Button>
+                <Typography variant="h5" color="textSecondary">
+                    {i +1}
+                </Typography>
+            </CardActions>
+        </Card>
+    )
+}
+
+
+export default Newscard
